3542429b3895b399a1b8680ae4ac32c1609d69bb
Daniil Shvalov <shvalovich@gmail.com>
2023-06-17 22:29:49 +0300

    feat(snippets): add cpp snippets

---
+++ luasnippets/cpp.lua
@@ -0,0 +1,27 @@
+local ls = require("luasnip")
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
+local extras = require("luasnip.extras")
+local l = extras.lambda
+local rep = extras.rep
+local fmt = require("luasnip.extras.fmt").fmt
+local fmta = require("luasnip.extras.fmt").fmta
+local postfix = require("luasnip.extras.postfix").postfix
+
+return {
+  s("in", fmt("#include <{}>", { i(1) })),
+  s("inn", fmt('#include "{}"', { i(1) })),
+  s("cout", fmt("std::cout << {} << std::endl;", { i(1) })),
+  s(
+    "main",
+    fmta(
+      [[
+        int main() {
+          <>
+        }
+      ]],
+      { i(0) }
+    )
+  ),
+}
